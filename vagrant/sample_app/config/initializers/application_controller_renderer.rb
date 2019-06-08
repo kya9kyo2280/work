f1fead61f36c98660a25cadba5294bea46d02942
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-06-08 18:36:46 +0900

    add5

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1e9f9b223adc0ff66d6cf7c5be93bc673964a6644ca06e7245e0664641ddcf22dc24be3439fac7b27d1a74763a89deec3e27e87cf12b7b14b1323e0097c0f123'
