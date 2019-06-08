1f30dbf15ce92e6c7ab3afcae4ce472730fb351f
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-06-08 19:41:55 +0900

    add6

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -26,6 +26,13 @@ class PostsController < ApplicationController
      redirect_to post_path(post.id)
    end
 
+   def destroy
+       post = Post.find(params[:id])
+       post.destroy
+       redirect_to posts_path
+
+   end
+
   private
 
 
